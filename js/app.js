83f62dd0d17abbeed1d05a6e8c7afb144c31530a
Luke Maser <lukemaser@gmail.com>
2015-05-19 15:09:38 -0400

    Added better hints.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
   		$('.overlay').fadeOut(1000);
   	});
     /*--- Debug help ---*/
-  	var DEBUG_MODE = false;
+  	var DEBUG_MODE = true;
   	var debug = function(msg) {
   		if (DEBUG_MODE == true) {
   			console.log('DEBUG:', msg);
@@ -44,18 +44,32 @@ $(document).ready(function(){
 		return randomNumber;
   	}
 
+  	/*--- Evaluate range of guess ---*/
+  		function between(x, min, max) {
+  			return x >= min && x <= max;
+  		}
+
   	/*--- Evaluate the user's guess against randomNumber ---*/
    	function evaluate(guess, random) {
-   		if (guess < random) {
+      if (guess == random) {
+        debug('Correct');
+        $('#guessList').html('<li>Correct</li>');
+      } else if (guess > (random - 10) && guess < (random + 10)) {
+        debug('Warmer');
+   			$('#guessList').html('<li>Warmer...</li>');
+   		} else if (guess > (random - 15) && guess < (random + 15)) {
+   			debug('Warm');
+   			$('#guessList').html('<li>Warm, guess again.</li>');
+   		} else if (guess > (random - 30) && guess < (random + 30)) {
+        debug('Cool');
+        $('#guessList').html('<li>Cool</li>');
+      } else if (guess > (random - 40) && guess <= (random + 40)) {
    			debug('Cold');
    			$('#guessList').html('<li>Cold</li>');
-   		} else if (guess > random) {
-   			debug('Hot');
-   			$('#guessList').html('<li>Hot</li>');
-   		} else if (guess == random) {
-   			debug('Correct');
-   			$('#guessList').html('<li>Correct</li>');
-   		};
+   		} else if (guess < random || guess > random) {
+        debug('Ice Cold');
+        $('#guessList').html('<li>Ice Cold</li>');
+      };
    	}
 
    	/*--- Click event for new game ---*/
@@ -66,14 +80,17 @@ $(document).ready(function(){
    		var guess = $('#userGuess').val();
    		/*--- Evaluate if guess is a string or number ---*/
             if (isNaN(guess)) {
-                alert("Please enter a integer");
+                alert("Please enter an integer");
                 $('#count').html('0');
             } else {
-            	evaluate(guess, randomNumber);
-
+            	
+            	//$('#guessList').append(guess + '<br/>');
             	$('#count').html(parseInt($('#count').html(), 10) +1);
             };  			
+            evaluate(guess, randomNumber);
    	});
+
+
 });
 
 
